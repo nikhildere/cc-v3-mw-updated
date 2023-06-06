6934f8bde38c1cdeeccf858971395b3030fe8a9d
nikhildere <nikhil.dere@live.com>
2023-06-06 23:57:09 +0530

    Update draftMessages.tsx

--- Source/CompanyCommunicator/ClientApp/src/components/DraftMessages/draftMessages.tsx
+++ Source/CompanyCommunicator/ClientApp/src/components/DraftMessages/draftMessages.tsx
@@ -4,7 +4,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { withTranslation, WithTranslation } from "react-i18next";
-import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
+//import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import { Loader, List, Flex, Text } from '@fluentui/react-northstar';
 import * as microsoftTeams from "@microsoft/teams-js";
 
@@ -59,7 +59,7 @@ class DraftMessages extends React.Component<IMessageProps, IMessageState> {
 
     constructor(props: IMessageProps) {
         super(props);
-        initializeIcons();
+        //initializeIcons();
         this.localize = this.props.t;
         this.isOpenTaskModuleAllowed = true;
         this.state = {
